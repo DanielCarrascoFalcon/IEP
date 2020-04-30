257c7e60f279fa4c1e803cc7397cdeacbab33157
Daniel Carrasco <drcarrasco@uc.cl>
2020-04-30 19:04:32 -0400

    connect backend

--- src/views/Tasks.js
+++ src/views/Tasks.js
@@ -1,13 +1,15 @@
 import React from "react";
 import Tasks from "../components/TasksList";
 
+const api = process.env.API_URL || "https://localhost:600/api";
+
 class Home extends React.Component {
   state = {
     tasks: [],
   };
 
   async componentDidMount() {
-    const response = await fetch(`/tasks`);
+    const response = await fetch(`${api}/tasks`);
     console.log("acaaaaaaaaaa1111", response);
     const json = await response.json();
     console.log("acaaaaaaaaaa", json);
