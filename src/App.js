73adbf641590a06121a6079586df819ef932a9c4
Daniel Carrasco <drcarrasco@uc.cl>
2020-04-30 14:58:07 -0400

    template

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React, { Component } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
-import "./assets/css/App.css";
-import PrimerComponente from "./components/PrimerComponente";
-import Navbar from "./components/Navbar";
-import Home from "./views/Home";
-import Tasks from "./views/Tasks";
-import NotFound from "./components/NotFound";
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/home" component={Home} />
-          <Route exact path="/tasks" component={Tasks} />
-          <Route component={NotFound} />
-        </Switch>
-      </BrowserRouter>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
